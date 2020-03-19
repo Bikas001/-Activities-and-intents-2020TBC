89ea6e0968c2d7706698a4fbc0e25736f6c8d088
Bikas001 <vcbikas123@gmail.com>
2020-03-19 13:38:09 +0545

    Adding Screenshot

--- app/src/main/java/np/com/cbikas/activitiesandintents2020/MainActivity.java
+++ app/src/main/java/np/com/cbikas/activitiesandintents2020/MainActivity.java
@@ -1,14 +1,69 @@
 package np.com.cbikas.activitiesandintents2020;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnsend;
+    TextView tvMassage,tvReply;
+    EditText etmsg;
+    private final String KEY_INTENT="ABC";
+    private final int REQUEST_KEY= 1;
+
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvMassage=findViewById(R.id.tvMassage);
+        tvReply=findViewById(R.id.tvReply);
+        etmsg=findViewById(R.id.etMsgsend);
+        tvReply.setVisibility(View.INVISIBLE);
+
+        tvMassage.setVisibility(View.INVISIBLE);
+
+
+    }
+
+
+    public void runSecondActivity(View view){
+
+        intent = new Intent(this, SecondActivity.class);
+        String msg=etmsg.getText().toString();
+        intent.putExtra(KEY_INTENT,msg);
+        startActivity(intent);
+        startActivityForResult(intent,REQUEST_KEY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_KEY){
+            if(resultCode==RESULT_OK){
+                String msg1=data.getStringExtra("ABCD");
+                tvReply.setVisibility(View.VISIBLE);
+
+                if(tvMassage == null){
+                    tvMassage.setText("This Message is Empty");
+                    tvMassage.setVisibility(View.VISIBLE);
+                }else{
+                    tvMassage.setText(msg1);
+                    tvMassage.setVisibility(View.VISIBLE);
+
+                }
+
+
+            }
+        }
+
+
     }
 }
--- app/src/main/java/np/com/cbikas/activitiesandintents2020/SecondActivity.java
+++ app/src/main/java/np/com/cbikas/activitiesandintents2020/SecondActivity.java
@@ -2,13 +2,57 @@ package np.com.cbikas.activitiesandintents2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ViewAnimator;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button btnmsgReply;
+    TextView tvMsg,tvMassageRecived;
+    EditText etMsgReply;
+    private final String KEY_INTENT="ABC";
+    private final String KEY_INTENT1="ABCD";
+    private final int REQUEST_KEY=1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        tvMsg =findViewById(R.id.tvMsg);
+        tvMassageRecived=findViewById(R.id.tvmassageRecevied);
+        etMsgReply=findViewById(R.id.etMsgReply);
+
+
+        Intent intent = getIntent();
+        String msg= intent.getStringExtra(KEY_INTENT);
+        tvMassageRecived.setText(msg);
+
+        if(tvMassageRecived.equals("")){
+            tvMassageRecived.setText("This Message is Empty");
+            tvMassageRecived.setVisibility(View.VISIBLE);
+        }else{
+            tvMassageRecived.setText(msg);
+            tvMassageRecived.setVisibility(View.VISIBLE);
+
+        }
+
+
     }
+
+    public void replyMessage(View view){
+      String msgrep=etMsgReply.getText().toString();
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(KEY_INTENT1, msgrep);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+
+    }
+
 }
